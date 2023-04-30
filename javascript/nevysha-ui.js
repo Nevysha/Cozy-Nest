36d2c635c0e584ada61d009b4687058cb6a657c0
Nevysha <me@nevysha.art>
2023-04-30 17:41:06 +0200

    wrap settings in a scrollable container

--- javascript/nevysha-ui.js
+++ javascript/nevysha-ui.js
@@ -1,7 +1,61 @@
 console.log("nevysha-ui.js")
 
+const wrapDataGenerationInfo = () => {
+  // Get the generation info container
+  const previewBlocks = document.querySelectorAll('#tab_txt2img div#txt2img_results > *:not(#txt2img_results)');
+  const generationInfoContainer = previewBlocks[1].children[2];
+
+  // Create the new container element and add a class for styling
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('preview-block-wrapper');
+
+  // Create the show/hide button element and add a click event listener
+  const toggleButton = document.createElement('button');
+  toggleButton.id = 'toggleGenInfoButton';
+  toggleButton.classList.add('nevysha', 'lg', 'primary', 'gradio-button', 'btn');
+  toggleButton.textContent = 'Show/Hide Generation Info';
+  toggleButton.addEventListener('click', () => {
+    generationInfoContainer.style.display = generationInfoContainer.style.display === 'none' ? '' : 'none';
+  });
+
+  // Add the toggle button and generation info container to the wrapper
+  wrapper.appendChild(toggleButton);
+  wrapper.appendChild(generationInfoContainer);
+
+  // Add the wrapper container at the end of the previewBlocks[1] container
+  previewBlocks[1].appendChild(wrapper);
+
+  // Hide the generation info container by default
+  generationInfoContainer.style.display = 'none';
+
+  // Remove the inline style from the previewBlocks[1] container
+  previewBlocks[1].style = "";
+}
+
+function wrapSettings() {
+
+  const settingsContainer = document.getElementById('txt2img_settings');
+
+  // Get its children elements
+  const settingsChildren = settingsContainer.querySelectorAll(':scope > :not(#txt2img_toprow)');
+
+  // Create the new container element and add a class for styling
+  const wrapperSettings = document.createElement('div');
+  wrapperSettings.classList.add('nevysha', 'settings-wrapper');
+
+  // Loop through the children elements starting from the second one
+  for (let child of settingsChildren) {
+    // Move each child into the new wrapper container
+    wrapperSettings.appendChild(child);
+  }
+
+  // Insert the wrapper container after the original container
+  settingsContainer.appendChild(wrapperSettings);
+}
+
 const onload = () => {
 
+  let gradioApp = window.gradioApp;
   if (typeof gradioApp !== "function") {
     console.log("not ready")
     setTimeout(onload, 2000);
@@ -46,36 +100,8 @@ const onload = () => {
   const settingsContainer = document.getElementById('txt2img_settings');
   settingsContainer.insertBefore(topRow, settingsContainer.firstChild);
 
-
-  // Get the generation info container
-  const previewBlocks = document.querySelectorAll('#tab_txt2img div#txt2img_results > *:not(#txt2img_results)');
-  const generationInfoContainer = previewBlocks[1].children[2];
-
-  // Create the new container element and add a class for styling
-  const wrapper = document.createElement('div');
-  wrapper.classList.add('preview-block-wrapper');
-
-  // Create the show/hide button element and add a click event listener
-  const toggleButton = document.createElement('button');
-  toggleButton.id = 'toggleGenInfoButton';
-  toggleButton.classList.add('nevysha', 'lg', 'primary', 'gradio-button', 'btn');
-  toggleButton.textContent = 'Show/Hide Generation Info';
-  toggleButton.addEventListener('click', () => {
-    generationInfoContainer.style.display = generationInfoContainer.style.display === 'none' ? '' : 'none';
-  });
-
-  // Add the toggle button and generation info container to the wrapper
-  wrapper.appendChild(toggleButton);
-  wrapper.appendChild(generationInfoContainer);
-
-  // Add the wrapper container at the end of the previewBlocks[1] container
-  previewBlocks[1].appendChild(wrapper);
-
-  // Hide the generation info container by default
-  generationInfoContainer.style.display = 'none';
-
-  // Remove the inline style from the previewBlocks[1] container
-  previewBlocks[1].style = "";
+  wrapSettings();
+  wrapDataGenerationInfo();
 
   console.log("nevysha-ui.js: DOMContentLoaded")
 };
