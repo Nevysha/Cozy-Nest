e141c4acda9751eadc9cd20cf7eba0301ef33620
Nevysha <me@nevysha.art>
2023-05-04 21:52:20 +0200

    draggable settings

--- javascript/nevysha-cozy-nest.js
+++ javascript/nevysha-cozy-nest.js
@@ -3,6 +3,7 @@ console.log("nevysha-ui.js")
 const waves = "<div><div class='wave'></div> <div class='wave'></div><div class='wave'></div></div>";
 const svg_magic_wand = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M234.7 42.7L197 56.8c-3 1.1-5 4-5 7.2s2 6.1 5 7.2l37.7 14.1L248.8 123c1.1 3 4 5 7.2 5s6.1-2 7.2-5l14.1-37.7L315 71.2c3-1.1 5-4 5-7.2s-2-6.1-5-7.2L277.3 42.7 263.2 5c-1.1-3-4-5-7.2-5s-6.1 2-7.2 5L234.7 42.7zM46.1 395.4c-18.7 18.7-18.7 49.1 0 67.9l34.6 34.6c18.7 18.7 49.1 18.7 67.9 0L529.9 116.5c18.7-18.7 18.7-49.1 0-67.9L495.3 14.1c-18.7-18.7-49.1-18.7-67.9 0L46.1 395.4zM484.6 82.6l-105 105-23.3-23.3 105-105 23.3 23.3zM7.5 117.2C3 118.9 0 123.2 0 128s3 9.1 7.5 10.8L64 160l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L128 160l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L128 96 106.8 39.5C105.1 35 100.8 32 96 32s-9.1 3-10.8 7.5L64 96 7.5 117.2zm352 256c-4.5 1.7-7.5 6-7.5 10.8s3 9.1 7.5 10.8L416 416l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L480 416l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L480 352l-21.2-56.5c-1.7-4.5-6-7.5-10.8-7.5s-9.1 3-10.8 7.5L416 352l-56.5 21.2z"/></svg>`;
 const loading_roll = `<div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>`
+const svg_draggable_anchor = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 9.4C21.9-3.1 42.1-3.1 54.6 9.4L160 114.7V96c0-17.7 14.3-32 32-32s32 14.3 32 32v96c0 4.3-.9 8.5-2.4 12.2c-1.6 3.7-3.8 7.3-6.9 10.3l-.1 .1c-3.1 3-6.6 5.3-10.3 6.9c-3.8 1.6-7.9 2.4-12.2 2.4H96c-17.7 0-32-14.3-32-32s14.3-32 32-32h18.7L9.4 54.6C-3.1 42.1-3.1 21.9 9.4 9.4zM256 256a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zM114.7 352H96c-17.7 0-32-14.3-32-32s14.3-32 32-32h96 0l.1 0c8.8 0 16.7 3.6 22.5 9.3l.1 .1c3 3.1 5.3 6.6 6.9 10.3c1.6 3.8 2.4 7.9 2.4 12.2v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V397.3L54.6 502.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L114.7 352zM416 96c0-17.7 14.3-32 32-32s32 14.3 32 32v18.7L585.4 9.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3L525.3 160H544c17.7 0 32 14.3 32 32s-14.3 32-32 32H448c-8.8 0-16.8-3.6-22.6-9.3l-.1-.1c-3-3.1-5.3-6.6-6.9-10.3s-2.4-7.8-2.4-12.2l0-.1v0V96zM525.3 352L630.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L480 397.3V416c0 17.7-14.3 32-32 32s-32-14.3-32-32V320v0c0 0 0-.1 0-.1c0-4.3 .9-8.4 2.4-12.2c1.6-3.8 3.9-7.3 6.9-10.4c5.8-5.8 13.7-9.3 22.5-9.4c0 0 .1 0 .1 0h0 96c17.7 0 32 14.3 32 32s-14.3 32-32 32H525.3z"/></svg>`
 
 const getTheme = () => {
   const gradioURL = window.location.href
@@ -495,6 +496,57 @@ function tweakNevyUiSettings() {
   });
 }
 
+const makeSettingsDraggable = () => {
+  // Get a reference to the draggable div element
+  const draggableSettings = document.querySelector('#nevyui_sh_options_panel');
+
+  // Define variables to keep track of the mouse position and offset
+  let isDragging = false;
+  let mouseX = 0;
+  let mouseY = 0;
+  let offsetX = 0;
+  let offsetY = 0;
+
+  // create draggable icon
+  const draggableAnchorIcon = document.createElement('div');
+  draggableAnchorIcon.classList.add('nevysha-draggable-anchor-icon');
+  //add a drag icon
+  draggableAnchorIcon.innerHTML = svg_draggable_anchor;
+  // add the anchor to the start of the draggable div
+  draggableSettings.insertBefore(draggableAnchorIcon, draggableSettings.firstChild);
+  // create a blank div above the svg icon to catch for mousedown events
+  const draggableAnchor = document.createElement('div');
+  draggableAnchor.classList.add('nevysha-draggable-anchor');
+  // add the anchor to the start of the draggable div
+  draggableSettings.insertBefore(draggableAnchor, draggableSettings.firstChild);
+
+
+  // Add event listeners for mouse events
+  draggableAnchor.addEventListener('mousedown', function(event) {
+    // Set dragging flag and store mouse position and offset from element top-left corner
+    isDragging = true;
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    offsetX = draggableSettings.offsetLeft;
+    offsetY = draggableSettings.offsetTop;
+  });
+
+  document.addEventListener('mousemove', function(event) {
+    // If dragging, update element position based on mouse movement
+    if (isDragging) {
+      const deltaX = event.clientX - mouseX;
+      const deltaY = event.clientY - mouseY;
+      draggableSettings.style.left = (offsetX + deltaX) + 'px';
+      draggableSettings.style.top = (offsetY + deltaY) + 'px';
+    }
+  });
+
+  document.addEventListener('mouseup', function(event) {
+    // Reset dragging flag
+    isDragging = false;
+  });
+}
+
 const onload = (done) => {
 
   let gradioApp = window.gradioApp;
@@ -575,6 +627,9 @@ const onload = (done) => {
   else {
     document.querySelector("body").classList.remove("nevysha-light")
   }
+
+  //make settings draggable
+  makeSettingsDraggable();
 
   done();
 };
