9babfd4af99fb1997cb55161ed573119710c1d72
Nevysha <me@nevysha.art>
2023-05-28 20:26:47 +0200

    models

---
+++ cozy-nest-client/image-browser/src/Models.js
@@ -0,0 +1,8 @@
+import {createContext} from "react";
+
+export const ImagesContext = createContext([])
+export const ImageContext = createContext(null)
+
+function reduceImage(images, action) {
+
+}
