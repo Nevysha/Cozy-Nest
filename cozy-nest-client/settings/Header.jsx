7619daa35218c22f351cda1b85f923b9186bcad5
Nevysha <me@nevysha.art>
2023-06-04 12:24:09 +0200

    fix

--- cozy-nest-client/settings/Header.jsx
+++ cozy-nest-client/settings/Header.jsx
@@ -3,26 +3,32 @@ import {Row} from "../main/Utils.jsx";
 import './App.css'
 
 export function Header (props) {
-    return (
-      <div className="nevysha header">
-        <div className="nevysha title">
-          <h2>Nevysha's Cozy Nest</h2>
-          <span className="subtitle">Find your cozy spot on Auto1111's webui</span>
-        </div>
-        <Row>
-          <div className="btn-settings" onClick={props.onClickClose}>Close</div>
-        </Row>
-        <div className="container">
-          <div className="nevysha settings-nevyui-top"><p className="nevysha-reporting">Found a bug or want to ask for
-            a feature ? Please <a onClick={window.gatherInfoAndShowDialog} href="_blank" target="_blank">click
-              here to gather relevant info</a> then use <a href="https://www.reddit.com/r/NevyshaCozyNest/"
-                                                           target="_blank">this subreddit</a> or <a
-              href="https://github.com/Nevysha/Cozy-Nest" target="_blank">github</a>. You can also join this <a
-              href="https://discord.gg/yppzDXjT7S" target="_blank">discord server</a> to discuss about Cozy Nest
-          </p><p className="nevysha-emphasis">WARNING : Some visual settings are immediately applied but will not be
-            saved until you click "Save"</p></div>
-        </div>
+
+  const gatherInfoAndShowDialog = () => {
+    window.gatherInfoAndShowDialog()
+    props.onClickClose()
+  }
+
+  return (
+    <div className="nevysha header">
+      <div className="nevysha title">
+        <h2>Nevysha's Cozy Nest</h2>
+        <span className="subtitle">Find your cozy spot on Auto1111's webui</span>
       </div>
-    );
+      <Row>
+        <div className="btn-settings" onClick={props.onClickClose}>Close</div>
+      </Row>
+      <div className="container">
+        <div className="nevysha settings-nevyui-top"><p className="nevysha-reporting">Found a bug or want to ask for
+          a feature ? Please <a onClick={gatherInfoAndShowDialog} target="_blank">click
+            here to gather relevant info</a> then use <a href="https://www.reddit.com/r/NevyshaCozyNest/"
+                                                         target="_blank">this subreddit</a> or <a
+            href="https://github.com/Nevysha/Cozy-Nest" target="_blank">github</a>. You can also join this <a
+            href="https://discord.gg/yppzDXjT7S" target="_blank">discord server</a> to discuss about Cozy Nest
+        </p><p className="nevysha-emphasis">WARNING : Some visual settings are immediately applied but will not be
+          saved until you click "Save"</p></div>
+      </div>
+    </div>
+  );
 
 }
